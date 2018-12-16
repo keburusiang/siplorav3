a87e1c1c0be1065a8d16cbe09e2f8cbeebc4f6d1
Amiin Majiid Nugroho <amiinmajiidnugroho@gmail.com>
2018-12-16 08:39:07 +0700

    susulan

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -99,7 +99,7 @@
   <body>
 
     <!-- iki Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark" id="mainNav">
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
       <a class="navbar-brand" href="{{URL::to('/')}}">SIPLORA</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
