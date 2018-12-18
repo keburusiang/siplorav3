a6c18ec117f84464b7524e954143b3b9438d26a9
Fauzan Awanda Alviansyah <fauzanawanda@gmail.com>
2018-12-19 06:06:47 +0700

    Update lapor.blade.php

--- resources/views/laporan/lapor.blade.php
+++ resources/views/laporan/lapor.blade.php
@@ -96,24 +96,24 @@
 
         var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
 
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+        // if (navigator.geolocation) {
+        //   navigator.geolocation.getCurrentPosition(function(position) {
+        //     var pos = {
+        //       lat: position.coords.latitude,
+        //       lng: position.coords.longitude
+        //     };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(peta);
-            peta.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, peta.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, peta.getCenter());
-        }
+        //     infoWindow.setPosition(pos);
+        //     infoWindow.setContent('Location found.');
+        //     infoWindow.open(peta);
+        //     peta.setCenter(pos);
+        //   }, function() {
+        //     handleLocationError(true, infoWindow, peta.getCenter());
+        //   });
+        // } else {
+        //   // Browser doesn't support Geolocation
+        //   handleLocationError(false, infoWindow, peta.getCenter());
+        // }
 
 
         // even listner ketika peta diklik
